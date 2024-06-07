d9f71c2ef1775e0e84610909b2f878af9f8396c2
“DavyL0” <d4vyl0pes@gmail.com>
2024-06-06 23:17:22 -0300

    Foi adicionado o sistema de recebimento de pagamento e o sistema de guardagem de dados mas de os 2 de modo incompleto necessitando de melhoria na coleta de dados e base de informações

--- Sistema de Ingresso/src/Main.java
+++ Sistema de Ingresso/src/Main.java
@@ -1,3 +1,6 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.sql.SQLOutput;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -35,13 +38,54 @@ public class Main {
         System.out.println("Qual peça vc quer assistir 1, 2 ou 3");
         int pecaTeatro = sc.nextInt();
         System.out.println("Qual horario você deseja assistir:  Manhã(M), Tarde(T), Noite(N)");
-        byte horarioPeca = sc.nextByte();
+        String horarioPeca = sc.next();
         System.out.println("Selecione sua poltrona: Plateia A (PA), Plateia B (PB), Frisa (FA), Camarote (CA), Balcão Nobre (BN)");
-        byte poltrona = sc.nextByte();
+        String poltrona = sc.next();
+        System.out.println("Quantos lugares você deseja comprar");
+        int quantLugar = sc.nextInt();
+    }
+    //Realiza compra de ingressos
+    private static void  compraIngressos(String cpfCliente, int pecaEscolhida, String horarioEscolhido, String poltronaEscolhida, int quantLugar){
+        double valorTotal = 0;
+        if()
+        //resultado compra
+        System.out.println(cpfCliente + " você escolheu a peça "+ pecaEscolhida + " no horario "+ horarioEscolhido +
+                " e "+quantLugar+ " nas poltronas "+ poltronaEscolhida+ " valor total "+valorTotal);
+    }
+    //Guarda Dados das Pecas
+    private static void guardaDados(String cpfCliente, int pecaEscolhida, String horarioEscolhido, String poltronaEscolhida){
+        String[][] pecaUm  = new String[1][1];
+        String[][] pecaDos = {};
+        String[][] pecaTres  = {};
+        int c = 0;
+
+        if (pecaEscolhida == 1 ){
+            pecaUm[c][0] = cpfCliente;
+            pecaUm[c][1] = horarioEscolhido;
+            pecaUm[c][2] = poltronaEscolhida;
+        } else if (pecaEscolhida == 2) {
+            pecaDos[c][0] = cpfCliente;
+            pecaDos[c][1] = horarioEscolhido;
+            pecaDos[c][2] = poltronaEscolhida;
+        } else if (pecaEscolhida == 3) {
+            pecaTres[c][0] = cpfCliente;
+            pecaTres[c][1] = horarioEscolhido;
+            pecaTres[c][2] = poltronaEscolhida;
+        }
+
+        for(int i = 0; i < pecaUm.length; i++) {
+            for(int j = 0; j < pecaUm[i].length;j++){
+                System.out.printf("%d\t", pecaUm[i][j]);
+            }
+            System.out.println("");
+        }
+
+
     }
 
     public static void main(String[] args) {
-        checaCPF();
+        //checaCPF();
+
     }
 
 
